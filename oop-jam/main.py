449baa6725772195563690323af120dd2e060f17
rolaur <rolaur@ttu.ee>
2020-10-21 14:09:33 +0300

    OOP jam

--- oop-jam/main.py
+++ oop-jam/main.py
@@ -9,7 +9,7 @@ class Product:
         self.model = model
         self.price = price
         self.stock = stock
-        self._volume = 0  # ruumala, _on ees, sest konstruktoris seda pole, väärtus tuleb setterist (kui tuleb)
+        self._volume = 0  # ruumala, _on ees, sest konstruktoris seda pole, vaikimis väärtus, uus väärtus tuleb setterist (kui tuleb)
         self.category = category
         self.tags = tags  # Selle parameetri tüübiks on list
 
@@ -25,7 +25,7 @@ class Product:
 
     # Setter dekoraator selleks, et muuta erinevatest parameetritest moodustunud parameetrit ühe korraga ?
     # Et muuta parameetrit erilisel viisil?
-    # Et muuta privaatseid parameetreid.
+    # Et muuta privaatseid parameetreid?
     @volume.setter
     def volume(self, volume_string):
         a, b, c = volume_string.split('x')
@@ -36,26 +36,26 @@ class Product:
 if __name__ == '__main__':
 
     # Teen alustuseks kaks toodet (ehk kaks objekti, mille tüübiks on Product (mitte int, string vms))
+    # NB! Objekt ei pea tegelikult üldse olema muutujale omistatud! Neid võib niisama ka listi toppida :)
     jalgratas = Product('Valio', 'Speedster Gravel 10', 2299, 1, 'jalgrattad', ['sport', 'scott', 'rattad'])
     parka = Product('Didriksons', 'Dante', 569.95, 10, 'meeste parkad', ['meestele', 'talv'])
 
     # Nii saan Product tüüpi objektist küsida parameetreid. Näiteks muutujasse 'jalgratas' salvestatud toote brändi.
-    # NB! Objekt ei pea tegelikult üldse olema muutujale omistatud! Neid võib niisama ka listi toppida :)
-    print(f"Näide, kuidas saab toote andmeid pärida: {jalgratas.brand}")
+    print(f"01. Näide, kuidas saab toote andmeid pärida: {jalgratas.brand}")
 
     # Objektid saan panna ka listi või mistahes kollektsiooni...
     list_of_products = [jalgratas, parka]
 
     # ... ja seejärjel võtta kõigi listi pandud toodete brändid ja hinnad näiteks nii:
-    print("\nMeil on olemas järgmised tooted: ")
+    print("\n02. Meil on olemas järgmised tooted: ")
     for product in list_of_products:
         print(f"{product.product_name} - {product.price} eur")
 
-    print("\nKuna me märkasime, et jalgratta bränd ilmselgelt on vale (Valio), siis paneme rattale õige brändi (Scott): ")
+    print("\n03. Kuna me märkasime, et jalgratta bränd ilmselgelt on vale (Valio), siis paneme rattale õige brändi (Scott): ")
     jalgratas.brand = "Scott"
     print(f"{jalgratas.product_name}  - {product.price} eur")
 
-    print("\nNüüd, kui parandused on tehtud, vaatame, missuguseid tootesilte on kasutatud: ")
+    print("\n04. Nüüd, kui parandused on tehtud, vaatame, missuguseid tootesilte on kasutatud: ")
     list_of_tags = []
 
     parka.tags.append("jope")  # lisame ühe tag'i parkale juurde
@@ -66,3 +66,22 @@ if __name__ == '__main__':
             if tag not in list_of_products:
                 list_of_tags.append(tag)
     print(list_of_tags)  # Prindime välja listi, kus on unikaalsed tootesildid.
+
+
+    # Kuna muutujate nimede väljamõtlemine on pigem tüütu, siis võib tooteid luua ka ilma muutujata :)
+    list_of_products = [
+        Product('Scott', 'Speedster Gravel 10', 2299, 1, 'jalgrattad', ['sport', 'scott', 'rattad']),
+        Product('Asics', 'Gel-Kayano 27', 199, 5, 'jooksujalatsid', ['sport', 'jalatsid', 'mehed', 'nike']),
+        Product('Nike', 'Zoom Winflo 7', 114, 8, 'jooksujalatsid', ['sport', 'jalatsid', 'mehed', 'nike'])
+    ]
+
+    print("\n05. Teistmoodi tekitatud listis on järgmised tooted: ")
+
+    for product in list_of_products:
+        print(f"{product.product_name} - {product.price} eur, (laoseis {product.stock} tk)")
+
+    print("\n06. Küsime listist tooteid, mille bränd on Nike: ")
+
+    for product in list_of_products:
+        if (product.brand == "Nike"):
+            print(f"{product.product_name} - {product.price} eur, (laoseis {product.stock} tk)")
